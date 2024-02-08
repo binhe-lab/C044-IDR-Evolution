58a93296e7ac780d3de1c29c04060edf3464ac7b
Joshua Ayelazuno <jayelazuno@gmail.com>
2024-02-08 14:54:40 -0600

    made several changes

--- 20-collect-orthologs/output/Pho4-Homologs-fas/README.md
+++ 20-collect-orthologs/output/Pho4-Homologs-fas/README.md
@@ -1,6 +1,8 @@
-This folder contains single Pho4 blastp hit .fas sequence which were retrieved from the Shen et al 2018 ( (PMID: 30415838)
+20231212-343taxa-proteins-homologs.fas This folder contains single Pho4 blastp hit .fas sequence/ single blastp hits with a single bHLH domain which were retrieved from the Shen et al 2018 ( (PMID: 30415838). 
 The hits were obtained from an already orthomcl_out reciprocal blast file used in the genome assembly file containing the fasta sequences.
 The hits were filtered for seqeunces with a single hit and using the species_name@seq_identifier, were retrieved using the retrive_sequences.sh script
 The hits were filtered for seqeunces with a single hit and using the species_name@seq_identifier, were retrieved using the retrive_sequences.sh script, located under scripts
 These .fas sequence will be used for further analysis to explore the evolution of ScPho4 protein
-
+A hmmscan (details in script dir) was performed 231 blastp hits also had a single hmmscan bHLH hits
+20240201-54species-grt-1bHLH.txt is a subset 20231212-343taxa-proteins-homologs.fas, from hmmscan which returned > 1 bHLH domain hits and no other pfam hits. Upon domain examination, of domain architecture, these were added back to 20231212-343taxa-proteins-homologs.fas making 285 core species  
+20241212_blast_hitsseq_grtq.fasta - blastp hits, where a single species had more than 1 Ph04 hit sequences. 
--- 20-collect-orthologs/script/retrieve_sequences.sh
+++ 20-collect-orthologs/script/retrieve_sequences.sh
@@ -5,28 +5,24 @@
 # title: .sh to retrieve .fas of Pho4 blastp homologs from the 332 yeast genomes from Shen et al 2018 (PMID: 30415838)
 # use:retrive_sequences.sh
 #########################
-# the input file is a .csv file with species_name and seq_identifier from the blastp hits
-input1="/Users/jayelazuno/Desktop/Pho4_orthologs /orthomcl_output/blastp_results/species_names.csv"
+input_csv="blast_hit_grt1_seqnames.csv" # file containing the names of species and sequence identifiers only                                                 
+fasta_file="343taxa_proteins.fasta"
+output_folder="/Users/jayelazuno/Desktop/Pho4_orthologs /orthomcl_output/cd"
 
-# Fasta file in the .fas from the shen et al data
-input2="/Users/jayelazuno/Desktop/Pho4_orthologs /orthomcl_output/343taxa_protein.fasta"
+# Create the output folder if it doesn't exist
+mkdir -p "$output_folder"
 
-# Output folder
-orthologs_retrieved.fas="/Users/jayelazuno/Desktop/Pho4_orthologs /orthomcl_output/blastp_results/orthologs_retrieved.fas"
+# Read each line of the CSV file and perform the search
+while IFS=, read -r Species seq_identifier; do
+    # Concatenate species and seq_identifier with "@" to form the search string
+    search_string="${Species}@${seq_identifier}"
 
-# Create output folder if it doesn't exist
-#mkdir -p "$output_folder"
+    # Define the output file name
+    output_file="${output_folder}/${species}_${seq_identifier}.fas"
 
-# Read CSV file and extract species_name and seq_identifier
-while IFS=, read -r species_name seq_identifier; do
-output_label="${species_name}_${seq_identifier}"
-# the annotated fasta is labeled with species_name@seq_identifier 
-search_string="${species_name}@${seq_identifier}"
-grep -A 1 -wF ">$search_string" "$input2" > "${out_results}/${output_label}.fas"
-done < "$input"
+    # Perform the search and save the result to the output file
+    grep -A 1 -wF ">${search_string}" "$fasta_file" > "$output_file"
 
-    # Search for the sequence in the fasta file and save to output file
-    grep -A 1 -wF ">$seq_identifier" "$input2" > "$orhologs_retrieved.fas/${output_label}.fas"
-done < "$input1"
-
-echo "Sequences retrieval completed. Check the '$orthologs_retrieved.fas' folder for the output files."
+    # Print a message indicating the completion of the search for each sequence
+    echo "Retrieved sequence for: ${species}_${seq_identifier}"
+done < "$input_csv"
